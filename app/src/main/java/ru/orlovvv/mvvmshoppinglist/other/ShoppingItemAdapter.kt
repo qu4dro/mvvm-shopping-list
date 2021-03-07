50d717dae094898d6cab0f062165feb1e1e57470
unknown <qu4dro.sl@gmail.com>
2021-03-07 14:28:59 +0800

    Shopping item adapter created

---
+++ app/src/main/java/ru/orlovvv/mvvmshoppinglist/other/ShoppingItemAdapter.kt
@@ -0,0 +1,47 @@
+package ru.orlovvv.mvvmshoppinglist.other
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.recyclerview.widget.RecyclerView
+import ru.orlovvv.mvvmshoppinglist.R
+import ru.orlovvv.mvvmshoppinglist.data.db.entities.ShoppingItem
+import ru.orlovvv.mvvmshoppinglist.ui.shoppinglist.ShoppingViewModel
+import kotlinx.android.synthetic.main.shopping_item.view.*
+
+class ShoppingItemAdapter(var items: List<ShoppingItem>, var viewModel: ShoppingViewModel) :
+    RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent)
+        return ShoppingViewHolder(view)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val currentShoppingItem = items[position]
+        holder.itemView.tvName.text = currentShoppingItem.name
+        holder.itemView.tvAmount.text = "${currentShoppingItem.amount}"
+
+        holder.itemView.ivDelete.setOnClickListener {
+            viewModel.delete(currentShoppingItem)
+        }
+
+        holder.itemView.ivPlus.setOnClickListener {
+            currentShoppingItem.amount++
+            viewModel.upsert(currentShoppingItem)
+        }
+
+        holder.itemView.ivMinus.setOnClickListener {
+            if (currentShoppingItem.amount > 0) {
+                currentShoppingItem.amount--
+                viewModel.upsert(currentShoppingItem)
+            }
+        }
+    }
+
+    inner class ShoppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
