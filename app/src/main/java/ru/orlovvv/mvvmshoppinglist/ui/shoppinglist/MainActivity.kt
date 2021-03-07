67aabc65d9ad27a99225c44db4577d9d7d11893d
unknown <qu4dro.sl@gmail.com>
2021-03-07 12:05:35 +0800

    Main activity updated

--- app/src/main/java/ru/orlovvv/mvvmshoppinglist/ui/shoppinglist/MainActivity.kt
+++ app/src/main/java/ru/orlovvv/mvvmshoppinglist/ui/shoppinglist/MainActivity.kt
@@ -2,11 +2,21 @@ package ru.orlovvv.mvvmshoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import ru.orlovvv.mvvmshoppinglist.R
+import ru.orlovvv.mvvmshoppinglist.data.db.ShoppingDatabase
+import ru.orlovvv.mvvmshoppinglist.data.repositories.ShoppingRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val db = ShoppingDatabase(this)
+        val repository = ShoppingRepository(db)
+        val factory = ShoppingViewModelFactory(repository)
+        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
     }
 }
