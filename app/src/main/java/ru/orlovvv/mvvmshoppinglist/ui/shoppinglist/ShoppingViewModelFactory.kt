b025da9896f6e7f7f8e79cda0d49b4fb4a12a3f5
unknown <qu4dro.sl@gmail.com>
2021-03-07 12:01:09 +0800

    ShoppingViewModelFactory created

---
+++ app/src/main/java/ru/orlovvv/mvvmshoppinglist/ui/shoppinglist/ShoppingViewModelFactory.kt
@@ -0,0 +1,13 @@
+package ru.orlovvv.mvvmshoppinglist.ui.shoppinglist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.orlovvv.mvvmshoppinglist.data.repositories.ShoppingRepository
+
+@Suppress("UNCHECKED_CAST")
+class ShoppingViewModelFactory(private val repository: ShoppingRepository): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ShoppingViewModel(repository) as T
+    }
+}
