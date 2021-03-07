63397f903ddaee20e5f59ea405fe6aff22f524e1
unknown <qu4dro.sl@gmail.com>
2021-03-07 11:44:21 +0800

    Shopping repository created

---
+++ app/src/main/java/ru/orlovvv/mvvmshoppinglist/data/repositories/ShoppingRepository.kt
@@ -0,0 +1,13 @@
+package ru.orlovvv.mvvmshoppinglist.data.repositories
+
+import ru.orlovvv.mvvmshoppinglist.data.db.ShoppingDatabase
+import ru.orlovvv.mvvmshoppinglist.data.db.entities.ShoppingItem
+
+class ShoppingRepository(private val db: ShoppingDatabase) {
+
+    suspend fun upsert(item: ShoppingItem) = db.getShoppingDao().upsert(item)
+
+    suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)
+
+    suspend fun getAllShoppingItems() = db.getShoppingDao().getAllShoppingItems()
+}
